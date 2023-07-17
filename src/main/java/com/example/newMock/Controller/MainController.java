29b5568e8d4fe976f1ae08d35922830061d316dd
ilyadoesntneedwater <ilya-kaigorodov@mail.ru>
2023-07-17 14:42:26 +0300

    first commit

---
+++ src/main/java/com/example/newMock/Controller/MainController.java
@@ -0,0 +1,33 @@
+package com.example.newMock.Controller;
+import com.example.newMock.Model.RequestDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.math.BigDecimal;
+
+@RestController
+public class MainController {
+
+    private Logger log = LoggerFactory.getLogger(MainController.class);
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    @PostMapping(
+            value = "/info/postBalances",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object postBalances(@RequestBody RequestDTO requestDTO){
+        try {
+            String clientID = requestDTO.getClientId();
+            char firstDigit = clientID.charAt(0);
+            BigDecimal maxLimit;
+        }
+    }
+
+}
