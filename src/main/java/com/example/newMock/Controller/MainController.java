14bf71d2d36a4870ecab0b610dfbc9042a103e5d
ilyadoesntneedwater <ilya-kaigorodov@mail.ru>
2023-07-17 17:35:28 +0300

    OKwithGet

--- src/main/java/com/example/newMock/Controller/MainController.java
+++ src/main/java/com/example/newMock/Controller/MainController.java
@@ -32,31 +32,78 @@ public class MainController {
             String clientID = requestDTO.getClientId();
             char firstDigit = clientID.charAt(0);
             BigDecimal maxLimit;
+            int randomNum;
 
             if (firstDigit == '8') {
                 maxLimit = new BigDecimal(2000.00);
+                randomNum = ThreadLocalRandom.current().nextInt(0,maxLimit.intValue());
                 responseDTO.setCurrency("US");
             }
             else if (firstDigit == '9') {
                 maxLimit = new BigDecimal(1000.00);
+                randomNum = ThreadLocalRandom.current().nextInt(0,maxLimit.intValue());
                 responseDTO.setCurrency("EU");
             }
             else {
                 maxLimit = new BigDecimal(10000.00);
+                randomNum = ThreadLocalRandom.current().nextInt(0,maxLimit.intValue());
                 responseDTO.setCurrency("RU");
             }
 
-            int randomNum = ThreadLocalRandom.current().nextInt(0,maxLimit.intValue());
             responseDTO.setRqUID(requestDTO.getRqUID());
             responseDTO.setClientId(clientID);
             responseDTO.setAccount(requestDTO.getAccount());
             responseDTO.setBalance(Integer.toString(randomNum));
             responseDTO.setMaxLimit(maxLimit);
-            log.error("***** Request ******" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
-            log.error("***** Response ******" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+            log.error("\n***** Request ******\n " + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
+            log.error("\n***** Response ******\n " + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
             return responseDTO;
-        } catch(Exception e) {
+        }
+        catch(Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 
+    @GetMapping(
+            value = "/info/getBalances",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object getBalances(@RequestBody RequestDTO requestDTO){
+        ResponseDTO responseDTO = new ResponseDTO();
+        try {
+            String clientID = requestDTO.getClientId();
+            char firstDigit = clientID.charAt(0);
+            BigDecimal maxLimit;
+            int randomNum;
+
+            if (firstDigit == '8') {
+                maxLimit = new BigDecimal(2000.00);
+                randomNum = ThreadLocalRandom.current().nextInt(0,maxLimit.intValue());
+                responseDTO.setCurrency("US");
+            }
+            else if (firstDigit == '9') {
+                maxLimit = new BigDecimal(1000.00);
+                randomNum = ThreadLocalRandom.current().nextInt(0,maxLimit.intValue());
+                responseDTO.setCurrency("EU");
+            }
+            else {
+                maxLimit = new BigDecimal(10000.00);
+                randomNum = ThreadLocalRandom.current().nextInt(0,maxLimit.intValue());
+                responseDTO.setCurrency("RU");
+            }
+
+            responseDTO.setRqUID(requestDTO.getRqUID());
+            responseDTO.setClientId(clientID);
+            responseDTO.setAccount(requestDTO.getAccount());
+            responseDTO.setBalance(Integer.toString(randomNum));
+            responseDTO.setMaxLimit(maxLimit);
+            log.error("\n***** Request ******\n " + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
+            log.error("\n***** Response ******\n " + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+            return responseDTO;
+        }
+        catch(Exception e)
+        {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
